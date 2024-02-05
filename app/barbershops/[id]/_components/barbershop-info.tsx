47b38a6c5ec2469a42eacb1d12182a465178204c
PietroKucharski <pietrokucharski@hotmail.com>
2024-02-04 23:55:31 -0300

    feat: add Barbershop info component

---
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -0,0 +1,68 @@
+"use client"
+
+import Image from "next/image"
+import {
+    ChevronLeftIcon,
+    MapPin,
+    MapPinIcon,
+    MenuIcon,
+    StarIcon,
+} from "lucide-react"
+import { Button } from "../../../_components/ui/button"
+import { Barbershop } from "@prisma/client"
+import { useRouter } from "next/navigation"
+
+interface BarbershopInfoProps {
+    barbershop: Barbershop
+}
+
+const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
+    const router = useRouter()
+
+    const handleBackClick = () => {
+        router.back()
+    }
+    return (
+        <div>
+            <div className="h-[256px] w-full relative">
+                <Button
+                    variant="outline"
+                    size="icon"
+                    className="z-50 top-4 left-4 absolute"
+                    onClick={handleBackClick}
+                >
+                    <ChevronLeftIcon />
+                </Button>
+
+                <Button
+                    variant="outline"
+                    size="icon"
+                    className="z-50 top-4 right-4 absolute"
+                >
+                    <MenuIcon />
+                </Button>
+                <Image
+                    alt={barbershop.name}
+                    src={barbershop.imageUrl}
+                    fill
+                    style={{ objectFit: "cover" }}
+                    className="opacity-75"
+                />
+            </div>
+
+            <div className="px-5 pt-3 pb-6 border-b border-solid border-secondary">
+                <h1 className="text-xl font-bold">{barbershop.name}</h1>
+                <div className="flex items-center gap-1 mt-2">
+                    <MapPinIcon className="text-primary" size={18} />
+                    <p className="text-sm">{barbershop.address}</p>
+                </div>
+                <div className="flex items-center gap-1 mt-2">
+                    <StarIcon className="text-primary" size={18} />
+                    <p className="text-sm">5,0 (889 avaliações)</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BarbershopInfo
