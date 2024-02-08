5c6bc09ca52219e4e67cff28a6a153d366355ad6
PietroKucharski <pietrokucharski@hotmail.com>
2024-02-08 10:58:30 -0300

    feat: ensure confirmation is shown on booking cancelation

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -45,16 +45,18 @@ export default async function Home() {
                 <Search />
             </div>
 
-            <div className="mt-6">
-                <h2 className="pl-5 text-xs uppercase text-gray-400 font-bold mb-3">
-                    Agendamentos
-                </h2>
-                <div className="flex px-5 gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-                    {confirmedBookings.map((booking) => (
-                        <BookingItem key={booking.id} booking={booking} />
-                    ))}
+            {confirmedBookings.length > 0 && (
+                <div className="mt-6">
+                    <h2 className="pl-5 text-xs uppercase text-gray-400 font-bold mb-3">
+                        Agendamentos
+                    </h2>
+                    <div className="flex px-5 gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+                        {confirmedBookings.map((booking) => (
+                            <BookingItem key={booking.id} booking={booking} />
+                        ))}
+                    </div>
                 </div>
-            </div>
+            )}
 
             <div className="mt-6">
                 <h2 className="text-xs uppercase text-gray-400 font-bold mb-3 px-5">
