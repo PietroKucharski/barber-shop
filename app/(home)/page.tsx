59ad85cc977cf893c32d1d53a1c586b2e324b387
PietroKucharski <pietrokucharski@hotmail.com>
2024-02-08 08:43:55 -0300

    feat: ensure confirmed bookings are shown on home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,9 +5,28 @@ import Search from "./_components/search"
 import BookingItem from "../_components/booking-item"
 import { db } from "../_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../api/auth/[...nextauth]/route"
 
 export default async function Home() {
-    const barbershops = await db.barbershop.findMany({})
+    const session = await getServerSession(authOptions)
+    const [barbershops, confirmedBookings] = await Promise.all([
+        db.barbershop.findMany({}),
+        session?.user
+            ? db.booking.findMany({
+                  where: {
+                      userId: (session.user as any).id,
+                      date: {
+                          gte: new Date(),
+                      },
+                  },
+                  include: {
+                      service: true,
+                      barbershop: true,
+                  },
+              })
+            : Promise.resolve([]),
+    ])
 
     return (
         <div>
@@ -26,12 +45,16 @@ export default async function Home() {
                 <Search />
             </div>
 
-            {/* <div className="px-5 mt-6">
-                <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">
+            <div className="mt-6">
+                <h2 className="pl-5 text-xs uppercase text-gray-400 font-bold mb-3">
                     Agendamentos
                 </h2>
-                <BookingItem />
-            </div> */}
+                <div className="flex px-5 gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+                    {confirmedBookings.map((booking) => (
+                        <BookingItem key={booking.id} booking={booking} />
+                    ))}
+                </div>
+            </div>
 
             <div className="mt-6">
                 <h2 className="text-xs uppercase text-gray-400 font-bold mb-3 px-5">
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -6,17 +6,20 @@ import { Button } from "./ui/button"
 import { MenuIcon } from "lucide-react"
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
 import SideMenu from "./side-menu"
+import Link from "next/link"
 
 const Header = () => {
     return (
         <Card>
             <CardContent className="p-5 justify-between items-center flex flex-row">
-                <Image
-                    src="/logo.png"
-                    alt="FSW Barber"
-                    height={22}
-                    width={120}
-                />
+                <Link href="/">
+                    <Image
+                        src="/logo.png"
+                        alt="FSW Barber"
+                        height={22}
+                        width={120}
+                    />
+                </Link>
                 <Sheet>
                     <SheetTrigger asChild>
                         <Button variant="outline" size="icon">
@@ -24,7 +27,7 @@ const Header = () => {
                         </Button>
                     </SheetTrigger>
                     <SheetContent className="p-0">
-                        <SideMenu/>
+                        <SideMenu />
                     </SheetContent>
                 </Sheet>
             </CardContent>
